38821cf2a879cec4c88f5298eca3feb74825e6ee
Liduen <raphael.orieux@gmail.com>
2022-04-07 16:42:26 +0200

    début du nouveau setup

---
+++ TestsPOC/src/main/java/timeline/MainTimeline.java
@@ -0,0 +1,69 @@
+package timeline;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class MainTimeline extends Application {
+	
+	
+	
+	private Stage primaryStage;
+	private AnchorPane mainLayout;
+	private boolean hitSpace = false;
+
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("TestTimeline");
+		
+		// Initialisation et ouverture de la fenètre
+		initMainLayout();
+		
+		
+	}
+	
+	public void initMainLayout() {
+		try {
+			// Chargement du layout principal
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainTimeline.class.getResource("GameTest.fxml"));
+			mainLayout = (AnchorPane) loader.load();
+			
+			// Affichage de la scène contenant le layout précédemment chargé
+			Scene scene = new Scene(mainLayout);
+			primaryStage.setScene(scene);
+			
+			// Mise en relation avec le controller
+			final Controller controller = loader.getController();
+			controller.setMainTimeline(this);
+			
+			primaryStage.show();
+			
+			
+			scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+				public void handle(KeyEvent event) {
+					if(event.getCode() == KeyCode.SPACE) {
+						hitSpace = true;
+						System.out.println("space");
+						controller.jumpOn();
+					}
+				};
+			});
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
