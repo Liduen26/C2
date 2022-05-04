2f52643f06fdb62e4e5766785c3736c496dc42b3
Robin <palmierobin@outlook.fr>
2022-05-04 11:51:03 +0200

    implementation variable / notion distance

--- TestTimeline2/src/timeline/Controller.java
+++ TestTimeline2/src/timeline/Controller.java
@@ -1,10 +1,11 @@
 package timeline;
 
+import java.awt.geom.Point2D;
+
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
-import javafx.geometry.Point2D;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
@@ -26,9 +27,15 @@ public class Controller {
 	long oldTime;
 	long newTime;
 	long dt;
+	double dp;
 	int frame;
 	long time;
 	boolean jump = false;
+	
+	// init centre cube
+			double centreX ;
+			double centreY;
+
 
 	public void setMainTimeline(MainTimeline main) {
 		mainApp = main;
@@ -42,23 +49,28 @@ public class Controller {
 		time = System.currentTimeMillis();
 
 		// init centre cube
-		double centreX = square.getLayoutX() + square.getWidth() / 2;
-		double centreY = square.getLayoutY() + square.getHeight() / 2;
+		 centreX = square.getLayoutX() + square.getWidth() / 2;
+		 centreY = square.getLayoutY() + square.getHeight() / 2;
 
 
 		//init vecteur vitesse
-		Point2D p1 = new Point2D(square.getLayoutX() + 50, centreY);
+		Point2D p1 = new Point2D.Double(centreX + 50, centreY);
 		Line vVitesse = new Line(centreX,centreY,p1.getX(),p1.getY());
 		vVitesse.setStroke(Color.RED);
+		
+		
 		//init vecteur gravité
-		Point2D p2 = new Point2D(centreX, square.getLayoutY() + 50);
+		Point2D p2 = new Point2D.Double(centreX, centreY + 50);
 		Line vG = new Line(centreX,centreY,p2.getX(),p2.getY());
 		vG.setStroke(Color.GREEN);
 
 		rootLayout.getChildren().add(vVitesse);
 		rootLayout.getChildren().add(vG);
-
-
+		
+		System.out.println("coo centre carée: " + centreX);
+		dp = p1.distance(centreX,centreY);
+		System.out.println("dp: "+dp);
+		
 		// init timeline	
 		Timeline timel = new Timeline(new KeyFrame(Duration.millis(1000 / 60), e -> {
 			// Calculs FPS
@@ -71,7 +83,22 @@ public class Controller {
 			square.setLayoutX(square.getLayoutX() + 1);
 			vVitesse.setLayoutX(vVitesse.getLayoutX() + 1);
 			vG.setLayoutX(vG.getLayoutX() + 1);
+			
+			
+			// init centre cube
+		 centreX = square.getLayoutX() + square.getWidth() / 2;
+		 centreY = square.getLayoutY() + square.getHeight() / 2;
 
+			p1.setLocation(p1.getX() + 1, centreY);
+			p2.setLocation(centreX, p2.getY());
+			System.out.println("coo J1: "+centreX + " // "+"next coo: " + p1.getX());
+		//	System.out.println("coo centre carée: " + centreX);
+			System.out.println("dp: "+dp);
+
+
+			
+			dp = p1.distance(centreX,centreY);
+			System.out.println(dp);
 
 			// faire sauter
 			if(jump == true) {
@@ -94,6 +121,7 @@ public class Controller {
 	}
 
 	public void jump() { jump = true; }
+	// lie le taille vvitesse avec vitesse et temps avec distance via la liaison precedente
 
 
 }
