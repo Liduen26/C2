5fe3cd13ab65e7cc1710d1213cdcb4e1cb8b2561
Liduen <raphael.orieux@gmail.com>
2022-05-05 09:43:35 +0200

    début de saut

--- TestTimeline2/src/timeline/Controller.java
+++ TestTimeline2/src/timeline/Controller.java
@@ -34,8 +34,8 @@ public class Controller {
 	double vitesse;
 	double distance;
 	
-	//pour changer la vitesse
-	final int constV = 50; 
+	//pour changer la vitsse
+	final int constV = 30; 
 	
 	// init var centre cube
 	double centreX ;
@@ -63,10 +63,14 @@ public class Controller {
 		Line vVitesse = new Line(centreX,centreY,p1.getX(),p1.getY());
 		vVitesse.setStroke(Color.RED);
 		
-		
 		//init vecteur gravité
 		Point2D p2 = new Point2D.Double(centreX, centreY + 50);
 		Line vG = new Line(centreX,centreY,p2.getX(),p2.getY());
+		vG.setStroke(Color.PURPLE);
+
+		//init vecteur vertical
+		Point2D p3 = new Point2D.Double(centreX, centreY);
+		Line vVert = new Line(centreX,centreY,p3.getX(),p3.getY());
 		vG.setStroke(Color.GREEN);
 
 		rootLayout.getChildren().add(vVitesse);
@@ -77,14 +81,29 @@ public class Controller {
 		
 		// init timeline	
 		Timeline timel = new Timeline(new KeyFrame(Duration.millis(1000 / 142), e -> {
-			
-			affFPS();
-			temps = dt / 1000000000; //dt par sec
-
-		
 			// init centre cube
 			centreX = square.getLayoutX() + square.getWidth() / 2;
 			centreY = square.getLayoutY() + square.getHeight() / 2;
+			
+			double verticalVelocity = centreY;
+			double gravity = p2.distance(centreX, centreY);
+			double jumpForce = 40;
+			
+			
+			dt = affFPS();
+			temps = dt / 1000000000; //dt par sec
+		
+			
+			
+			// faire sauter
+			if(jump == true) {
+				verticalVelocity = jumpForce;
+			}
+			
+			p3.setLocation(centreX, p3.getY() - verticalVelocity);
+			vVert.setLayoutY(p3.getY());
+			
+			System.out.println("x : " + vVert.getLayoutX() + " / y : " + vVert.getLayoutY());
 			
 			
 			// distance vect entre centre du joueur et le point (vitesse)
@@ -93,26 +112,29 @@ public class Controller {
 			
 			distance = vitesse * temps;
 			
-			// faire avancer					
+			// Déplacements X			
 			square.setLayoutX(square.getLayoutX() + distance);
 			vVitesse.setLayoutX(vVitesse.getLayoutX() + distance);
 			vG.setLayoutX(vG.getLayoutX() + distance);
+			vVert.setLayoutX(vVert.getLayoutX() + distance);
+			
+			// Déplacements Y
+//			square.setLayoutX(square.getLayoutX() + distance);
+//			vVitesse.setLayoutX(vVitesse.getLayoutX() + distance);
+//			vG.setLayoutX(vG.getLayoutX() + distance);
 			
 			//actualiser position des points 
 			p1.setLocation(p1.getX() + distance, centreY);
 			p2.setLocation(centreX, p2.getY() + distance);
 
-			System.out.println("coordonée: " + centreX);
-			System.out.println("distance: "+ distance);
-			System.out.println("Temps : " + temps + "\n dt : " + dt + "\n");
-
-
+//			System.out.println("coordonée: " + centreX);
+//			System.out.println("distance: "+ distance);
+//			System.out.println("Temps : " + temps + "\n dt : " + dt + "\n");
 
 					
-			// faire sauter
-			if(jump == true) {
-				
-			}
+			
+			
+			
 		}));
 
 		//TL luncher
@@ -133,7 +155,9 @@ public class Controller {
 			fps.setText("FPS : " + frame);
 			frame = 0;
 			time = System.currentTimeMillis();				
-		} return dt;
+		} 
+		
+		return dt;
 	}
 
 	public void jump() { jump = true; }
