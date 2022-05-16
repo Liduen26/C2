465a14df77383cb45a6bc4ca22fbbb01982c8655
Snowky92 <amaurypinheiro@hotmail.fr>
2022-05-16 15:50:04 +0200

    Menu + launch button

--- c²/src/application/Main.java
+++ c²/src/application/Main.java
@@ -1,26 +1,46 @@
 package application;
-	
+
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import test.adressApp.adress.view.RootLayoutController;
 
 
 public class Main extends Application {
+
+	private Stage primaryStage;
+	private Pane BaseMenu;
+	// ... AFTER THE OTHER VARIABLES ...
+	
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,500,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("C²");
+		
+		// Set the application icon.
+//        this.primaryStage.getIcons().add(new Image("file:ressources/images/21254.png"));
+
+		initBaseMenu();
 	}
-	
-	public static void main(String[] args) {
-		launch(args);
+
+public void initBaseMenu() {
+	try {
+		// Load root layout from fxml file.
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("/application/BaseMenu.fxml"));
+		BaseMenu = (Pane) loader.load();
+
+		// Show the scene containing the root layout.
+		Scene scene = new Scene(BaseMenu);
+		primaryStage.setScene(scene);
+
+		primaryStage.show();
+	} catch (IOException e) {
+		e.printStackTrace();
 	}
+}
 }
