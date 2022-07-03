39b92e7ec7d3fead93fc26fb3acdc352890ec75d
Snowky92 <amaurypinheiro@hotmail.fr>
2022-07-03 23:07:38 +0200

    modif chemin

--- src/cCarre/AffichageMap/view/MainController.java
+++ src/cCarre/AffichageMap/view/MainController.java
@@ -142,7 +142,7 @@ public class MainController {
 		//JSON parser object to parse read file
         JSONParser jsonParser = new JSONParser();
          
-        try (FileReader reader = new FileReader("C:\\Users\\amaur\\eclipse-workspace\\c²/Color.json"))
+        try (FileReader reader = new FileReader("./Color.json"))
         {
             //Read JSON file
             Object obj = jsonParser.parse(reader);
